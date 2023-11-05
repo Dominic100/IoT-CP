a478787c83a9daa8953e5c7cfb666b230594cabb
Dominic100 <51508258+Dominic100@users.noreply.github.com>
2023-11-05 16:31:29 +0530

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,246 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <HTTPSRedirect.h>
+
+#include<Wire.h>
+
+//---------------------------------------------------------------------------------------------------------
+// Enter Google Script Deployment ID:
+const char *GScriptId = "AKfycbzF3NciNxcdl2pJPZV281Xb7WG2kpgXrM1MOqnj6-X8m2GGywprmvdVNjoGg-neFL0clA";
+String gate_number = "Gate1";
+//---------------------------------------------------------------------------------------------------------
+// Enter network credentials:
+const char* ssid     = "Aneesh's Galaxy S21 FE 5G";
+const char* password = "okvs2599";
+//---------------------------------------------------------------------------------------------------------
+// Enter command (insert_row or append_row) and your Google Sheets sheet name (default is Sheet1):
+String payload_base =  "{\"command\": \"insert_row\", \"sheet_name\": \"Sheet1\", \"values\": ";
+String payload = "";
+//---------------------------------------------------------------------------------------------------------
+// Google Sheets setup (do not edit)
+const char* host        = "script.google.com";
+const int   httpsPort   = 443;
+const char* fingerprint = "";
+String url = String("/macros/s/") + GScriptId + "/exec";
+HTTPSRedirect* client = nullptr;
+//------------------------------------------------------------
+// Declare variables that will be published to Google Sheets
+String rfid;
+//------------------------------------------------------------
+int blocks[] = {4,5,6,8,9,10};
+#define total_blocks  (sizeof(blocks) / sizeof(blocks[0]))
+//------------------------------------------------------------
+#define RST_PIN  0  //D3
+#define SS_PIN   2  //D4
+#define BUZZER   4  //D2
+//------------------------------------------------------------
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;  
+MFRC522::StatusCode status;
+//------------------------------------------------------------
+/* Be aware of Sector Trailer Blocks */
+int blockNum = 2;  
+/* Create another array to read data from Block */
+/* Legthn of buffer should be 2 Bytes more than the size of Block (16 Bytes) */
+byte bufferLen = 18;
+byte readBlockData[18];
+const int buttonPin = D2;
+int buttonPressCount = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  //----------------------------------------------------------
+  Serial.begin(9600);        
+  delay(10);
+  Serial.println('\n');
+  //----------------------------------------------------------
+  SPI.begin();
+  //----------------------------------------------------------
+  WiFi.begin(ssid, password);             
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println('\n');
+  Serial.println("WiFi Connected!");
+  //Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+  //----------------------------------------------------------
+  // Use HTTPSRedirect class to create a new TLS connection
+  client = new HTTPSRedirect(httpsPort);
+  client->setInsecure();
+  client->setPrintResponseBody(true);
+  client->setContentTypeHeader("application/json");
+  //----------------------------------------------------------
+  delay(5000);
+  //----------------------------------------------------------
+  Serial.print("Connecting to ");
+  Serial.println(host);
+  //----------------------------------------------------------
+  // Try to connect for a maximum of 5 times
+  bool flag = false;
+  for(int i=0; i<5; i++){ 
+    int retval = client->connect(host, httpsPort);
+    //*************************************************
+    if (retval == 1){
+      flag = true;
+      String msg = "Connected. OK";
+      Serial.println(msg);
+      break;
+    }
+    //*************************************************
+    else
+      Serial.println("Connection failed. Retrying...");
+    //*************************************************
+  }
+  //----------------------------------------------------------
+  if (!flag){
+    //____________________________________________
+    Serial.print("Could not connect to server: ");
+    Serial.println(host);
+    delay(5000);
+    return;
+    //____________________________________________
+  }
+  //----------------------------------------------------------
+  delete client;    // delete HTTPSRedirect object
+  client = nullptr; // delete HTTPSRedirect object
+  //----------------------------------------------------------
+}
+
+void loop() {
+  //Serial.println("[TEST] loop() starts");
+  //----------------------------------------------------------------
+  static bool flag = false;
+
+  if (digitalRead(buttonPin) == HIGH) {
+    // Button is pressed, increment the button press count
+    buttonPressCount++;
+    Serial.println("Incremented by 1");
+  }
+
+  if (!flag){
+    client = new HTTPSRedirect(httpsPort);
+    client->setInsecure();
+    flag = true;
+    client->setPrintResponseBody(true);
+    client->setContentTypeHeader("application/json");
+  }
+  if (client != nullptr){
+    //when below if condition is TRUE then it takes more time then usual, It means the device 
+    //is disconnected from the google sheet server and it takes time to connect again
+    //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+    if (!client->connected()){
+      int retval = client->connect(host, httpsPort);
+      if (retval != 1){
+        Serial.println("Disconnected. Retrying...");
+        return; //Reset the loop
+      }
+    }
+    //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+  }
+  else{Serial.println("Error creating client object!"); Serial.println("else");}
+  //----------------------------------------------------------------
+  //Serial.println("[TEST] Scan Your Tag");
+  /* Initialize MFRC522 Module */
+  mfrc522.PCD_Init();
+  /* Look for new cards */
+  /* Reset the loop if no new card is present on RC522 Reader */
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {return;}
+  /* Select one of the cards */
+  if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
+  /* Read data from the same block */
+  Serial.println();
+  Serial.println(F("Reading last data from RFID..."));  
+  //----------------------------------------------------------------
+  String values = "", data;
+
+  for (byte i = 0; i < total_blocks; i++) {
+    ReadDataFromBlock(blocks[i], readBlockData);
+    //*************************************************
+    if(i == 0){
+      data = String((char*)readBlockData);
+      data.trim();
+      rfid = data;
+      values = "\"" + data + ",";
+    }
+    //*************************************************
+    /*else if(i == total_blocks-1){
+      data = String((char*)readBlockData);
+      data.trim();
+      values += data + "\"}";
+    }*/
+    //*************************************************
+    else{
+      data = String((char*)readBlockData);
+      data.trim();
+      values += data + ",";
+    }
+  }
+  values += String(buttonPressCount) + ",";
+  values += gate_number + "\"}";
+  //----------------------------------------------------------------
+  // Create json object string to send to Google Sheets
+  // values = "\"" + value0 + "," + value1 + "," + value2 + "\"}"
+  payload = payload_base + values;
+
+  // Publish data to Google Sheets
+  Serial.println("Publishing data...");
+  Serial.println(payload);
+  if(client->POST(url, host, payload)){ 
+    // do stuff here if publish was successful
+    Serial.println("[OK] Data published.");
+  }
+  //----------------------------------------------------------------
+  else{
+    // do stuff here if publish was not successful
+    Serial.println("Error while connecting");
+  }
+  //----------------------------------------------------------------
+  // a delay of several seconds is required before publishing again    
+  Serial.println("[TEST] delay(5000)");
+  delay(5000);
+}
+
+void ReadDataFromBlock(int blockNum, byte readBlockData[]) 
+{ 
+  //----------------------------------------------------------------------------
+  /* Prepare the ksy for authentication */
+  /* All keys are set to FFFFFFFFFFFFh at chip delivery from the factory */
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  //----------------------------------------------------------------------------
+  /* Authenticating the desired data block for Read access using Key A */
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNum, &key, &(mfrc522.uid));
+  //----------------------------------------------------------------------------s
+  if (status != MFRC522::STATUS_OK){
+     Serial.print("Authentication failed for Read: ");
+     Serial.println(mfrc522.GetStatusCodeName(status));
+     return;
+  }
+  //----------------------------------------------------------------------------
+  else {
+    Serial.println("Authentication success");
+  }
+  //----------------------------------------------------------------------------
+  /* Reading data from the Block */
+  status = mfrc522.MIFARE_Read(blockNum, readBlockData, &bufferLen);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("Reading failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+  //----------------------------------------------------------------------------
+  else {
+    readBlockData[16] = ' ';
+    readBlockData[17] = ' ';
+    Serial.println("Block was read successfully");  
+  }
+  //----------------------------------------------------------------------------
+}
